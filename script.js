366eb8f1b4d35b011d5369a7d4f76431f8cd24b5
lunartheboss <150518564+lunartheboss@users.noreply.github.com>
2023-11-11 16:22:11 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+function toggleSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.classList.toggle("open");
+}
+
+function leave() {
+  // Show loading screen for 3 seconds
+  setTimeout(() => {
+    window.location.href = "index.html";
+  }, 3000);
+}
+
+function validateTCInput(input) {
+  const tcWarningMsg = document.getElementById("tcWarningMsg");
+  if (!/^\d+$/.test(input.value)) {
+    tcWarningMsg.textContent = "Please enter only numbers.";
+  } else if (input.value.length < 12) {
+    tcWarningMsg.textContent = "Incorrect entry";
+  } else {
+    tcWarningMsg.textContent = "";
+  }
+}
+
+function validateTextInput(input) {
+  if (input.value.trim() === "") {
+    input.style.borderColor = "red";
+  } else {
+    input.style.borderColor = "";
+  }
+}
+
+function sorgula(type) {
+  const resultBox = document.getElementById("resultBox");
+  // Implement database interaction and display data in resultBox
+  resultBox.textContent = "Data will be displayed here"; // Replace with actual data
+}
